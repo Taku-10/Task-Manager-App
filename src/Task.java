7da642936c606a9be60eca20964bf3e93064f044
Takudzwa <takudzwamaseva020@gmail.com>
2023-07-28 10:11:41 +0200

    Task class

---
+++ src/Task.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task {
+    private String taskId;
+    private int timeToComplete;
+    private List<Task> dependencies;
+
+    public Task(String taskId, int timeToComplete) {
+        this.taskId = taskId;
+        this.timeToComplete = timeToComplete;
+        this.dependencies = new ArrayList<>();
+    }
+
+    public void setTaskId(String newTaskID) {
+        this.taskId = newTaskID;
+    }
+
+    public void setTimeToComplete(int time) {
+        this.timeToComplete = time;
+    }
+
+    public String getTaskId() {
+        return this.taskId;
+    }
+
+    public int getTimeToComplete() {
+        return this.timeToComplete;
+    }
+
+
+}
+
